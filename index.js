7c0def842d60fb5ef63dce3ccb3ba1aec84bb09e
shamim1202 <mehedi.farzu@gmail.com>
2025-11-03 22:50:13 +0600

    install & implement dotenv file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 const app = express();
@@ -8,8 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://smartDbUser:zbBZJFEBn415xn5d@cluster0.qx9mzcm.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qx9mzcm.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
