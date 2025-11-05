ca7f881bdd698df12e65416b3c627c34b86a73d2
shamim1202 <mehedi.farzu@gmail.com>
2025-11-06 01:16:52 +0600

    fixed firebase service key error

--- encode.js
+++ encode.js
@@ -1,4 +1,4 @@
-// encode.js
+// // encode.js
 const fs = require("fs");
 const key = fs.readFileSync("./smart-deals-firebase-adminsdk.json", "utf8");
 const base64 = Buffer.from(key).toString("base64");
--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const app = express();
 
 // Firebase Admin ------------------>
 const admin = require("firebase-admin");
-// index.js
 const decoded = Buffer.from(
   process.env.FIREBASE_SERVICE_KEY,
   "base64"
 ).toString("utf8");
 const serviceAccount = JSON.parse(decoded);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
